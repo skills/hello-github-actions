eb455caaad2c24aa0973ea946c8e883482706452
Kevin Heis <heiskr@users.noreply.github.com>
2022-02-03 13:26:11 -0800

    Update check-file.sh

--- .github/script/check-file.sh
+++ .github/script/check-file.sh
@@ -2,8 +2,11 @@
 # Make sure this file is executable
 # chmod a+x .github/script/check-file.sh
 
+# Make sure to escape your backslashes => \\ <= in YAML
+# So that its still a single \ in bash
+
 echo "Check that $FILE includes $SEARCH"
-if grep --extended-regexp $SEARCH $FILE
+if grep --extended-regexp "$SEARCH" -- $FILE
 then
   echo "Found $SEARCH in $FILE"
 else
